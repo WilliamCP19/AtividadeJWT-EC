5554f9e70ff476f63850f9dab53aec1b4942f137
WilliamCP <williamcruzpires@alunos.utfpr.edu.br>
2024-06-11 21:25:53 -0300

    Validando o JWT para Personalizar Resposta ao Usuário

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mustache = require('mustache-express');
-const tokenJWT = require('./rotas/auth');
+const express = require("express");
+const mustache = require("mustache-express");
+const tokenJWT = require("./rotas/auth");
 
 const servidor = express();
 
@@ -9,25 +9,41 @@ servidor.use(express.urlencoded({ extended: true }));
 
 // servidor.use('/auth', authRoutes);
 
-servidor.engine('mustache', mustache());
-servidor.set('view engine', 'mustache');
-servidor.set('views', __dirname + '/views');
+servidor.engine("mustache", mustache());
+servidor.set("view engine", "mustache");
+servidor.set("views", __dirname + "/views");
 
-servidor.get('/', (req, res) => {
-    res.render('gerarToken');
-})
+servidor.get("/", (req, res) => {
+    res.render("gerarToken");
+});
 
-servidor.get('/token', (req, res) => {
-    res.render('token')
-})
+servidor.get("/token", (req, res) => {
+    res.render("token");
+});
 
-servidor.post('/gerando-token', (req, res) => {
+servidor.post("/gerando-token", (req, res) => {
     const mensagem = req.body.msg;
-    res.render('token', {
-        token : tokenJWT(mensagem)
+    res.render("token", {
+        token: tokenJWT.gerarToken(mensagem)
     });
-})
+});
 
-servidor.listen (3000, () => {
-    console.log ("Rodando, na porta 3000...");
-});     
+servidor.post("/descriptografando-token", (req, res) => {
+    const token = req.body.token;
+    const mensagem = tokenJWT.descriptografarToken(token);
+    
+    if (mensagem == null) {
+        res.render('mensagem', {
+            mensagem: "Este Token NÂO é Válido!"
+        })
+    } else {
+        res.render('mensagem', {
+            mensagem: mensagem.message
+        })
+    }
+    
+});
+
+servidor.listen(3000, () => {
+    console.log("Rodando, na porta 3000...");
+});
--- rotas/auth.js
+++ rotas/auth.js
@@ -4,9 +4,23 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = '2313707';
 
 
-function gerarToken (mensagem) {
+function gerarToken(mensagem) {
     const token = jwt.sign({ message: mensagem }, SECRET_KEY, { expiresIn: '1h' });
     return token;
 }
 
-module.exports = gerarToken;
+function descriptografarToken(token) {
+
+    if (!token) {
+        return null;
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        return decoded;
+    } catch (err) {
+        return null;
+    }
+}
+
+module.exports = { gerarToken, descriptografarToken };
